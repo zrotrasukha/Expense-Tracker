72e36d344976db29acd9147ab2aa227473d49edb
zrotrasukha <zrotrasukha@gmail.com>
2025-07-02 14:17:57 +0530

    fix: update server to use Bun for handling requests

--- server/main.ts
+++ server/main.ts
@@ -4,5 +4,16 @@ const servehandler = (req: Request) => {
   return app.fetch(req);
 };
 
-console.log(`🦕 Server is running at http://localhost:4000`);
-Deno.serve({ port: 4000 }, servehandler);
+console.log(`🧈 Server is running at http://localhost:4000`);
+Bun.serve({
+  port: 4000,
+  fetch: servehandler,
+});
+// import app from "./app.ts";
+
+// const servehandler = (req: Request) => {
+//   return app.fetch(req);
+// };
+
+// console.log(`🦕 Server is running at http://localhost:4000`);
+// Deno.serve({ port: 4000 }, servehandler);
