870759917b2e68b3b47937f68e28298422888402
zrotrasukha <zrotrasukha@gmail.com>
2025-07-03 22:16:35 +0530

    chore: update dependencies and improve TypeScript configuration
    - Removed deprecated dependency "@hono/node-server" and added new dependencies for React Query and React Router.
    - Updated various dependencies to their latest versions for better performance and security.
    - Enhanced TypeScript configuration with new path mappings for easier imports.
    - Improved Vite configuration to include Tanstack Router plugin and updated alias paths.
    - Added path mappings in server's tsconfig for consistency across projects.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,24 +1,30 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import { defineConfig } from "vite"
-
 // https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [tanstackRouter({
+    target: 'react',
+    autoCodeSplitting: true,
+  }), react(), tailwindcss()],
   server: {
     port: 3000,
     host: true,
     strictPort: true,
-      proxy: {
-        "/api": {
-          target: "http://localhost:4000",
-        }
-      },
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+      }
     },
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@server": path.resolve(__dirname, "../server"),
+      "@components": path.resolve(__dirname, "./src/components"),
     },
-  })
+  },
+})
