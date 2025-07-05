d9aebdb24df65f9bc954c7289e068af414abf43c
zrotrasukha <zrotrasukha@gmail.com>
2025-07-05 09:06:51 +0530

    feat: add FieldInfo component for displaying field validation state

---
+++ client/src/lib/fieldInfo.tsx
@@ -0,0 +1,14 @@
+import type { AnyFieldApi } from "@tanstack/react-form";
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em>{field.state.meta.errors.join(', ')}</em>
+      ) : null}
+      {field.state.meta.isValidating ? 'Validating...' : null}
+    </>
+  )
+}
+
+export default FieldInfo;
