be7f58879a7dc8112fcbc4be0a47c9437e29a97f
zrotrasukha <zrotrasukha@gmail.com>
2025-07-05 09:06:07 +0530

    feat: refactor total expenses fetching to use React Query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from "react"
-import ExpenseTrackerCard from "./components/ExpenseTrackerCard"
+import ExpenseTrackerCard from "@components/ExpenseTrackerCard"
+import { api } from "@/lib/api"
+import { useQuery } from "@tanstack/react-query"
+
+async function getTotalSpent(){
+  const res = await api.expenses.total.$get();
+  if(!res.ok){
+    throw new Error("server error");
+  }
+  const data = await res.json();
+  return data; 
+}
 
 function App() {
-  const [totalSpent, setTotalSpent] = useState(0);
-  useEffect(() => {
-    try {
-      async function fetchTool() {
-        const response = await fetch('/api/expenses/total');
-        const data = await response.json();
-        setTotalSpent(data.total);
-        console.log('Total Expenses:', data.total);
-      }
-      fetchTool();
-    } catch (error) {
-      console.error('Error fetching total expenses:', error);
-    }
-  }, []);
-
-  return (
+  const {data, isPending, error} = useQuery({
+    queryKey: ['get-total-spent'],
+    queryFn: getTotalSpent
+  })
+  return(
     <div className="flex justify-center items-center h-screen w-screen bg-zinc-900">
-      <ExpenseTrackerCard totalSpent={totalSpent} /></div>
+      <ExpenseTrackerCard totalSpent={data?.total} error={error} isPending={isPending} />
+    </div>
   )
 }
 
