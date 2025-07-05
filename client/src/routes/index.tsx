53dea10b1037a988f5cd2fed9da1efd1e9cf9a96
zrotrasukha <zrotrasukha@gmail.com>
2025-07-05 09:07:22 +0530

    feat: add Index route with total spent calculation and display

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,30 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { api } from '@/lib/api';
+import ExpenseTrackerCard from '@/components/ExpenseTrackerCard';
+import { useQuery } from '@tanstack/react-query';
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+async function getTotalSpent(){
+  const res = await api.expenses.total.$get();
+  if(!res.ok){
+    throw new Error("server error");
+  }
+  const data = await res.json();
+  return data; 
+}
+
+function Index() {
+  const {data, isPending, error} = useQuery({
+    queryKey: ['get-total-spent'],
+    queryFn: getTotalSpent
+  })
+  return(
+    <div className="flex justify-center items-center h-screen w-screen bg-zinc-900">
+      <ExpenseTrackerCard totalSpent={data?.total} error={error} isPending={isPending} />
+    </div>
+  )
+}
+
+export default Index
