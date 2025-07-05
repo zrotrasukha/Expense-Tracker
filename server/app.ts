04e89a81dd479363c148982778ec6c7e2a8f8f17
zrotrasukha <zrotrasukha@gmail.com>
2025-07-05 09:07:25 +0530

    refactor: replace inline CORS configuration with external CorsConfig utility

--- server/app.ts
+++ server/app.ts
@@ -2,16 +2,17 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expenseRouter } from "./router/expenseRouter";
 import { serveStatic } from 'hono/bun'
-import { cors } from 'hono/cors';
+import { CorsConfig } from "@/utils/corsConfig";
 
 const app = new Hono();
-app.use('/api/*', cors({
-  origin: '*'
-}));
+app.use('/api/*', CorsConfig);
+
+const apiRoute = app.basePath('/api')
+    .route("/expenses", expenseRouter);
 app.use("*", logger());
-app.use('*', serveStatic({ root: './client/dist' }))
+app.get('*', serveStatic({ root: './client/dist' }))
 app.get('*', serveStatic({ path: './client/dist/index.html' }))
-app.route("/api/expenses", expenseRouter);
 
 
+export type apiRoute = typeof apiRoute;
 export default app;
